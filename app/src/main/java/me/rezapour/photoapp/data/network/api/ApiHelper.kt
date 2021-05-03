a132fc326eed3dd45a34184583f5907a81f39f7e
Mohammadreza Abdollahpour <reza.pour1992@gmail.com>
2021-05-03 13:34:41 +0430

    fit:(PhotosListFragment->add unsplash dataModel and config recyclerview)

--- app/src/main/java/me/rezapour/photoapp/data/network/api/ApiHelper.kt
+++ app/src/main/java/me/rezapour/photoapp/data/network/api/ApiHelper.kt
@@ -1,8 +1,9 @@
 package me.rezapour.photoapp.data.network.api
 
-import me.rezapour.photoapp.data.network.models.PhotosData
+
+import me.rezapour.photoapp.data.network.models.UnSplashSearchResult
 import retrofit2.Response
 
 interface ApiHelper {
-    suspend fun getPhotos(token: String): Response<List<PhotosData>>
+    suspend fun getPhotos(token: String): Response<List<UnSplashSearchResult>>
 }
